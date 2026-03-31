28b4b2ceb3ab952f8d4e79774d51274dc543a787
dakshvanshaj <davevanshaj55646@gmail.com>
2026-03-31 17:27:37 +0000

    chore: add path's to bronze layer data

--- config.py
+++ config.py
@@ -6,4 +6,9 @@ PROJECT_ROOT = Path(__file__).parent.resolve()
 # ============ RAW DATA PATH ============
 RAW_SALES_PATH = PROJECT_ROOT / "data" / "raw" / "sales.csv"
 RAW_FEATURES_PATH = PROJECT_ROOT / "data" / "raw" / "features.csv"
-RAW_STORES_PATH = PROJECT_ROOT / "data" / "raw" / "stores.csv"
+RAW_STORES_PATH  = PROJECT_ROOT / "data" / "raw" / "stores.csv"
+
+# ============ BRONZE DATA PATH ============
+BRONZE_SALES_PATH = PROJECT_ROOT / "data" / "bronze" / "sales"
+BRONZE_FEATURES_PATH = PROJECT_ROOT / "data" / "bronze" / "features"
+BRONZE_STORES_PATH = PROJECT_ROOT / "data" / "bronze" / "stores"
