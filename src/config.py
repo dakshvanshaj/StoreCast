a7bdf1019a09067829ac919f2e05fe080c167df5
dakshvanshaj <davevanshaj55646@gmail.com>
2026-04-08 14:59:52 +0000

    chore: update config

--- src/config.py
+++ src/config.py
@@ -1,22 +1,25 @@
 from pathlib import Path
 
 # ============ Project Root =============
-PROJECT_ROOT = Path(__file__).parent.resolve()
+PROJECT_ROOT = Path(__file__).parent.parent.resolve()
+
+# ============ DATA DIRECTORY ============
+DATA_DIR = PROJECT_ROOT / "data"
 
 # ============ RAW DATA PATH ============
-RAW_SALES_PATH = PROJECT_ROOT / "data" / "raw" / "sales.csv"
-RAW_FEATURES_PATH = PROJECT_ROOT / "data" / "raw" / "features.csv"
-RAW_STORES_PATH  = PROJECT_ROOT / "data" / "raw" / "stores.csv"
+RAW_SALES_PATH = DATA_DIR / "raw" / "sales.csv"
+RAW_FEATURES_PATH = DATA_DIR / "raw" / "features.csv"
+RAW_STORES_PATH  = DATA_DIR / "raw" / "stores.csv"
 
 # ============ BRONZE DATA PATH ============
-BRONZE_SALES_PATH = PROJECT_ROOT / "data" / "bronze" / "sales"
-BRONZE_FEATURES_PATH = PROJECT_ROOT / "data" / "bronze" / "features"
-BRONZE_STORES_PATH = PROJECT_ROOT / "data" / "bronze" / "stores"
+BRONZE_SALES_PATH = DATA_DIR / "bronze" / "sales"
+BRONZE_FEATURES_PATH = DATA_DIR / "bronze" / "features"
+BRONZE_STORES_PATH = DATA_DIR / "bronze" / "stores"
 
 # ============ SILVER DATA PATH ============
-SILVER_SALES_PATH = PROJECT_ROOT / "data" / "silver" / "sales"
-SILVER_FEATURES_PATH = PROJECT_ROOT / "data" / "silver" / "features"
-SILVER_STORES_PATH = PROJECT_ROOT / "data" / "silver" / "stores"
+SILVER_SALES_PATH = DATA_DIR / "silver" / "sales"
+SILVER_FEATURES_PATH = DATA_DIR / "silver" / "features"
+SILVER_STORES_PATH = DATA_DIR / "silver" / "stores"
 
 # ============ GOLD DATA PATH ============
-GOLD_MASTER_PATH = PROJECT_ROOT / "data" / "gold" / "master_sales.parquet"
+GOLD_MASTER_PATH = DATA_DIR / "gold" / "master_sales.parquet"
